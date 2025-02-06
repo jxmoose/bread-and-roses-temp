45b152d3836e616d710071af485f29deac7176b3
jxmoose <justin.xue7@gmail.com>
2025-02-06 10:48:47 -0800

    fix: typo + removed a county

--- app/onboarding/show-preference/page.tsx
+++ app/onboarding/show-preference/page.tsx
@@ -38,13 +38,12 @@ const facilityTypeOptions = new Set([
 
 const locationOptions = new Set([
   'Alameda',
-  'Contra Coast',
+  'Contra Costa',
   'Marin',
   'Napa',
   'San Fransisco',
   'San Mateo',
   'Santa Clara',
-  'Solano',
   'Sonoma',
 ]);
 
